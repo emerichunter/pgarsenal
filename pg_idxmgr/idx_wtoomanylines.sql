5b1218733b6c24a771dcb7805a475c4c621043f1
emerichunter <areyoudeadyet667@yahoo.fr>
2017-07-19 17:34:49 +0200

    corrected ERROR:  division by zero
    corrected ERROR:  division by zero when relation has no tuple

--- pg_idxmgr/idx_wtoomanylines.sql
+++ pg_idxmgr/idx_wtoomanylines.sql
@@ -12,6 +12,7 @@ SELECT pg_sui.relname, indexrelname, idx_scan, idx_tup_read, idx_tup_fetch, pg_c
                 FROM pg_stat_user_indexes pg_sui, pg_class pg_c
                 WHERE pg_c.relname = pg_sui.relname
                 AND idx_scan <> 0
+                AND pg_c.reltuples <> 0
                 AND idx_tup_read/pg_c.reltuples/idx_scan  > 0.5
                 GROUP BY pg_sui.relname, indexrelname, idx_scan, idx_tup_read, idx_tup_fetch, idx_tup_fetch/idx_scan*100, pg_c.reltuples
                 HAVING (idx_tup_read/idx_scan> 1)
