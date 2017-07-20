2ef0603a0dd044840bb1dd21678636794f3bb791
emerichunter <areyoudeadyet667@yahoo.fr>
2017-07-20 11:54:24 +0200

    Delete ideal_sharedbuffers.sql

--- pg_perfadvisor/ideal_sharedbuffers.sql
+++
@@ -1,9 +0,0 @@
-SELECT pg_size_pretty(count(*) * 8192) as ideal_shared_buffers 
- 
-FROM pg_class c 
- 
-INNER JOIN pg_buffercache b ON b.relfilenode = c.relfilenode 
- 
-INNER JOIN pg_database d ON (b.reldatabase = d.oid AND d.datname = current_database()) 
- 
-WHERE usagecount >= 3;
