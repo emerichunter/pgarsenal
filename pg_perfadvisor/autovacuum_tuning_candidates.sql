cabb04d479d7c05f7d3e51fec2d03df05322a3ec
emerichunter <areyoudeadyet667@yahoo.fr>
2017-10-06 15:53:34 +0200

    Update autovacuum_tuning_candidates.sql

--- pg_perfadvisor/autovacuum_tuning_candidates.sql
+++ pg_perfadvisor/autovacuum_tuning_candidates.sql
@@ -3,14 +3,14 @@
 -- a table above the thresahold and with a lot of activity could find it hard to autovacuum
 -- you should then consider these tables for a little tuning of autovacuum
 
-SELECT psut.relname,
+SSELECT psut.relname,
      to_char(psut.last_vacuum, 'YYYY-MM-DD HH24:MI') as last_vacuum,
      to_char(psut.last_autovacuum, 'YYYY-MM-DD HH24:MI') as last_autovacuum,
-     to_char(pg_class.reltuples, '9G999G999G999') AS n_tup,
-     to_char(psut.n_dead_tup, '9G999G999G999') AS dead_tup,
-     to_char(CAST(current_setting('autovacuum_vacuum_threshold') AS bigint)
+     to_char(pg_class.reltuples,  '9G999G999G999')  AS n_tup,
+     psut.n_dead_tup AS dead_tup,
+     (CAST(current_setting('autovacuum_vacuum_threshold') AS bigint)
          + (CAST(current_setting('autovacuum_vacuum_scale_factor') AS numeric)
-            * pg_class.reltuples), '9G999G999G999') AS av_threshold,
+            * pg_class.reltuples)) AS av_threshold,
      CASE
          WHEN CAST(current_setting('autovacuum_vacuum_threshold') AS bigint)
              + (CAST(current_setting('autovacuum_vacuum_scale_factor') AS numeric)
@@ -20,5 +20,5 @@ SELECT psut.relname,
      END AS expect_av
  FROM pg_stat_all_tables psut
      JOIN pg_class on psut.relid = pg_class.oid
- ORDER BY 1;
+ORDER BY  n_dead_tup DESC;
  
